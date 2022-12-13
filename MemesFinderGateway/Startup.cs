e7979f78ace09338948c5e1dca291d4aa10e956b
Artem Kiyashko <artem86@hotmail.co.uk>
2022-12-13 20:39:14 +0100

    add Startup and key vault configuration

---
+++ MemesFinderGateway/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Azure.Identity;
+using MemesFinderGateway.Extensions;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(MemesFinderGateway.Startup))]
+namespace MemesFinderGateway
+{
+	public class Startup : FunctionsStartup
+    {
+        private IConfigurationRoot _functionConfig;
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .AddEnvironmentVariables();
+
+            if (builder.IsProduction())
+                configBuilder.AddAzureKeyVault(
+                    new Uri($"https://{builder.GetContext().Configuration["KeyVaultName"]}.vault.azure.net/"),
+                    new DefaultAzureCredential());
+
+            _functionConfig = configBuilder.Build();
+
+            builder.Services.AddLogging();
+        }
+    }
+}
+
